53ed6245298f0980bec1cb673e8f47efabe62492
ruesin <ruesin@gmail.com>
2019-01-15 11:10:47 +0800

    add samples

---
+++ samples/config/redis.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'default' => [
+        'host'      => '127.0.0.1',
+        'port'      => '6379',
+        'database'  => '0',
+        'username'  => '',
+        'password'  => '',
+        'prefix'    => 'default:'
+    ],
+    'web' => [
+        'host'      => '127.0.0.1',
+        'port'      => '6379',
+        'database'  => '0',
+        'username'  => '',
+        'password'  => '',
+        'prefix'    => 'web:'
+    ]
+];
